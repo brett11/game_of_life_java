902db88b579570fca5db1811ab6edb063b4ccadf
Brett F <brettfox11@gmail.com>
2018-01-10 19:42:57 -0600

    More formatting changes

--- src/main/java/com/brett/model/GridHelpers.java
+++ src/main/java/com/brett/model/GridHelpers.java
@@ -1,14 +1,13 @@
 package com.brett.model;
 
 public class GridHelpers {
-    public static int[][] TRADITIONAL_NEIGHBOR_COORDINATES =
-            // clockwise
-    // 0, 0   0, 1   0, 2
-    // 1, 0   1, 1   1, 2
-    // 2, 0   2, 1   2, 2
-            {
-            {-1,-1}, {-1, 0}, {-1, 1},
-            { 0,-1},          { 0, 1},
-            { 1,-1}, { 1, 0}, { 1, 1}
-            };
+    public static int[][] TRADITIONAL_NEIGHBOR_COORDINATES = {
+        // clockwise
+        // 0, 0   0, 1   0, 2
+        // 1, 0   1, 1   1, 2
+        // 2, 0   2, 1   2, 2
+        {-1, -1}, {-1, 0}, {-1, 1},
+        { 0, -1},          { 0, 1},
+        { 1, -1}, { 1, 0}, { 1, 1}
+        };
 }
